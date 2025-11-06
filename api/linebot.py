f16a8b9230977f00af9813f508f0f8c8cb089f38
eric202501 <eric202502@gmail.com>
2025-11-06 14:34:19 +0800

    update

--- api/linebot.py
+++ api/linebot.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
-                            CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
-                            ImageSendMessage, ButtonsTemplate, ConfirmTemplate)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 import os
-import requests
-from bs4 import BeautifulSoup
-import random
-import html
+import html  # 使用標準庫的 escape 函數
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
@@ -35,31 +30,12 @@ def callback():
 def handle_message(event):
     if event.message.text.lower() == "test":
         reply_message = "第32行的reply_message=改成自己想傳送的訊息"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
-    if event.message.text.lower() == "button":
-        try:
-            buttons_template = TemplateSendMessage(
-                alt_text='buttons template',
-                template=ButtonsTemplate(
-                    thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                    title='Brown Cafe',
-                    text='Enjoy your coffee',
-                    actions=[
-                        MessageAction(
-                            label='咖啡有什麼好處',
-                            text='讓人有精神'),
-                        URIAction(
-                            label='伯朗咖啡',
-                            uri='https://www.mrbrown.com.tw/')
-                    ]
-                )
-            )
-            line_bot_api.reply_message(event.reply_token, buttons_template)
-        except Exception as e:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=str(e)))
-        
-  
+    else:
+        reply_message = event.message.text
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message))
+
 if __name__ == "__main__":
     app.run()
