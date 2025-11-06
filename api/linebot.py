0e7c401c572b1be44779b3004914470f38b50038
eric202501 <eric202502@gmail.com>
2025-11-06 14:57:35 +0800

    update

--- api/linebot.py
+++ api/linebot.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
                             CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
-                            ImageSendMessage, ButtonsTemplate)
+                            ImageSendMessage, ButtonsTemplate, ConfirmTemplate)
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -48,20 +48,105 @@ def random_ptt_boards():
 
     return board_info
 
-def movie_rank(url):
-    soup = BeautifulSoup(requests.get(url).text)
-    first = soup.find('dl', class_ = 'rank_list_box').find('h2').text
-    movie_rank = '第1名：' + first + '\n'
-
-    movie_list = soup.find_all('div', class_ = 'rank_txt')
-    for index, info in enumerate(movie_list):
-        movie = info.text
-        movie_rank += '第{}名：{}\n'.format(str(index + 2), movie)
-
-    return movie_rank
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    if event.message.text == 'confirm':
+        confirm_template = TemplateSendMessage(
+            alt_text = 'confirm template',
+            template = ConfirmTemplate(
+                text = 'drink coffee?',
+                actions = [
+                    MessageAction(
+                        label = 'yes',
+                        text = 'yes'),
+                    MessageAction(
+                        label = 'no',
+                        text = 'no')]
+                )
+            )
+        line_bot_api.reply_message(event.reply_token, confirm_template)
+
+
+    #按鈕樣板
+    if event.message.text == 'button':
+        buttons_template = TemplateSendMessage(
+            alt_text = 'buttons template',
+            template = ButtonsTemplate(
+                thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                title = 'Brown Cafe',
+                text = 'Enjoy your coffee',
+                actions = [
+                    MessageAction(
+                        label = '咖啡有什麼好處',
+                        text = '讓人有精神'),
+                    URIAction(
+                        label = '伯朗咖啡',
+                        uri = 'https://www.mrbrown.com.tw/')]
+                )
+            )
+
+        line_bot_api.reply_message(event.reply_token, buttons_template)
+
+
+    #carousel樣板
+    if event.message.text == 'carousel':
+        carousel_template = TemplateSendMessage(
+            alt_text = 'carousel template',
+            template = CarouselTemplate(
+                columns = [
+                    #第一個
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        title = 'this is menu1',
+                        text = 'menu1',
+                        actions = [
+                            MessageAction(
+                                label = '咖啡有什麼好處',
+                                text = '讓人有精神'),
+                            URIAction(
+                                label = '伯朗咖啡',
+                                uri = 'https://www.mrbrown.com.tw/')]),
+                    #第二個
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        title = 'this is menu2',
+                        text = 'menu2',
+                        actions = [
+                            MessageAction(
+                                label = '咖啡有什麼好處',
+                                text = '讓人有精神'),
+                            URIAction(
+                                label = '伯朗咖啡',
+                                uri = 'https://www.mrbrown.com.tw/')])
+                ])
+            )
+
+        line_bot_api.reply_message(event.reply_token, carousel_template)
+
+
+    #image carousel樣板
+    if event.message.text == 'image carousel':
+        image_carousel_template = TemplateSendMessage(
+            alt_text = 'image carousel template',
+            template = ImageCarouselTemplate(
+                columns = [
+                    #第一張圖
+                    ImageCarouselColumn(
+                        image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        action = URIAction(
+                            label = '伯朗咖啡',
+                            uri = 'https://www.mrbrown.com.tw/')),
+                    #第二張圖
+                    ImageCarouselColumn(
+                        image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        action = URIAction(
+                            label = '伯朗咖啡',
+                            uri = 'https://www.mrbrown.com.tw/'))                       
+                ])
+            )
+
+        line_bot_api.reply_message(event.reply_token, image_carousel_template)
 
     # 如果收到訊息是「PTT」就隨機選三個板
     if event.message.text == 'PTT':
@@ -96,7 +181,6 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, board_template)
 
-    
 
 if __name__ == "__main__":
     app.run()
