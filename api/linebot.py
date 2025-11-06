1495007726d3d522e69a863fb849d74b82952c02
eric202501 <eric202502@gmail.com>
2025-11-06 14:40:03 +0800

    update

--- api/linebot.py
+++ api/linebot.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+
+
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
+                            CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
+                            ImageSendMessage, ButtonsTemplate, ConfirmTemplate)
 import os
-import html  # 使用標準庫的 escape 函數
+
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
@@ -30,12 +34,35 @@ def callback():
 def handle_message(event):
     if event.message.text.lower() == "test":
         reply_message = "第32行的reply_message=改成自己想傳送的訊息"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_message))
+
+    elif event.message.text.lower() == "button":
+        try:
+            buttons_template = TemplateSendMessage(
+                alt_text='buttons template',
+                template=ButtonsTemplate(
+                    thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                    title='Brown Cafe',
+                    text='Enjoy your coffee',
+                    actions=[
+                        MessageAction(
+                            label='咖啡有什麼好處',
+                            text='讓人有精神'),
+                        URIAction(
+                            label='伯朗咖啡',
+                            uri='https://www.mrbrown.com.tw/')
+                    ]
+                )
+            )
+
+            # 嘗試回覆樣板訊息
+            line_bot_api.reply_message(event.reply_token, buttons_template)
+        except Exception as e:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(e)))
     else:
         reply_message = event.message.text
     
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 if __name__ == "__main__":
     app.run()
